7d5fb9b87ef6fd876c7fa005e868d1eac94253a5
victor_moreira <victormoreira@vivaldi.net>
2023-04-22 19:45:09 -0300

    foto adicionada

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,8 @@
 import styled from "styled-components"
+import Link from 'next/link'
 import React from "react"
+import { join } from "path"
+
 /*
 import {
 
@@ -14,14 +17,26 @@ font-size: 50px;
 // color: ${({ theme }) => theme.colors.primary};
 `
 
-export default function Home() {
+export default function Home(props) {
   return (
     <div>
-      <header>
-        <Title>Minha Página</Title>
-        <img src='...'/>
+      <header className="headerContainer">
+        <img src={props.avatar_url} />
+      <Link href='/sobre'>
+          <h1>Moreira blogue</h1>
+      </Link>  
 
       </header>
     </div>
     )
+}
+
+
+export async function getStaticProps(){
+  return {
+    props: {
+      avatar_url: "https://avatars.githubusercontent.com/u/125832442?v=4"
+    }
+  }
+
 }
