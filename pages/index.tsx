571695b8e7b3df12afaa974e762a80537177c8cd
victor_moreira <victormoreira@vivaldi.net>
2023-04-22 22:40:23 -0300

    bug porque nao tenho o link certo que leva aos favoritos do github

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,15 +3,6 @@ import Link from 'next/link'
 import React from "react"
 import { join } from "path"
 
-/*
-import {
-
-  Title,
-  
-} from '../components/sharedstyles'
-import Cards from '../components/cards'
-*/
-
 const Title = styled.h1`
 font-size: 50px;
 // color: ${({ theme }) => theme.colors.primary};
@@ -22,12 +13,38 @@ export default function Home(props) {
     <div>
       <header className="headerContainer">
         <img src={props.avatar_url} />
+        
       <Link href='/sobre'>
           <h1>Moreira blogue</h1>
       </Link>  
 
       </header>
+
+      <div>
+      <section className="postsContainer">
+        <h2>Repositórios Favoritos</h2>
+        {
+          props.repos.map((project) => {
+            return(
+              <article className="postsContainer__post">
+                <a href="/">
+                  {project.repo}
+                </a>
+                <p>
+                  {project.description}
+                </p>
+              </article>
+            )
+          })
+        }
+
+      </section>
     </div>
+
+    </div>
+
+    
+
     )
 }
 
@@ -36,10 +53,20 @@ export async function getStaticProps(){
 const gitRubResponse = await fetch("https://api.github.com/users/victormoreira")
 .then(res => res.json())
 
+
+
+const reposfav = await fetch('https://api.github.com/users/victormoreira')
+.then(res => res.json())
+
+
   return {
+    
     props: {
-      avatar_url: gitRubResponse.avatar_url
+      avatar_url: gitRubResponse.avatar_url,
+      reposfav,
+      
     }
+    
   }
 
 }
